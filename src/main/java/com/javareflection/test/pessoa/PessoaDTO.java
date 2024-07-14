c88a6d46a3ca58060ac1b5c9dcc8599a8916f7eb
KaianLuciano <kaianlucianoalvesdossantos@gmail.com>
2024-07-13 22:28:15 -0300

    Create entity PessoaDTO

--- src/main/java/com/javareflection/test/pessoa/PessoaDTO.java
+++ src/main/java/com/javareflection/test/pessoa/PessoaDTO.java
@@ -1,4 +1,15 @@
 package com.javareflection.test.pessoa;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class PessoaDTO {
+    private String nome;
+    private String cpf;
 }
