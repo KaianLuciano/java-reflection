07f36a85028cae94629ad49767ac355c7b98fb67
KaianLuciano <kaianlucianoalvesdossantos@gmail.com>
2024-07-13 23:13:30 -0300

    Create Endereco

---
+++ src/main/java/com/javareflection/test/endereco/Endereco.java
@@ -0,0 +1,11 @@
+package com.javareflection.test.endereco;
+
+public class Endereco {
+    public Endereco(String rua, Integer numero) {
+        this.rua = rua;
+        this.numero = numero;
+    }
+
+    private String rua;
+    private Integer numero;
+}
