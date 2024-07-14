9333a10615c00bade798d1e998900aa9d1ce1648
KaianLuciano <kaianlucianoalvesdossantos@gmail.com>
2024-07-13 22:29:23 -0300

    Create PessoaService

--- src/main/java/com/javareflection/test/pessoa/PessoaService.java
+++ src/main/java/com/javareflection/test/pessoa/PessoaService.java
@@ -1,4 +1,9 @@
 package com.javareflection.test.pessoa;
 
 public class PessoaService {
+    public PessoaDTO list() {
+        Pessoa pessoa = new PessoaRepository().list();
+        PessoaDTO pessoaDTO = new PessoaDTO(pessoa.getNome(), pessoa.getCpf());
+        return pessoaDTO;
+    }
 }
