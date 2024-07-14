7a1672dbae69c26bbbb6d742e7dcba280cc223f4
KaianLuciano <kaianlucianoalvesdossantos@gmail.com>
2024-07-13 23:13:18 -0300

    Update Pessoa

--- src/main/java/com/javareflection/test/pessoa/Pessoa.java
+++ src/main/java/com/javareflection/test/pessoa/Pessoa.java
@@ -1,16 +1,25 @@
 package com.javareflection.test.pessoa;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class Pessoa {
+    public Pessoa(Integer id, String nome, String cpf) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public Pessoa(String nome) {
+        this.nome = nome;
+    }
+
     private Integer id;
     private String nome;
     private String cpf;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
 }
